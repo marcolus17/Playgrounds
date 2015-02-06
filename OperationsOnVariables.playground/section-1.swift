c74500b19255661c44f720ee3e177c5b4ed17965
Nick Markworth <marcolus17@users.noreply.github.com>
2015-02-05 18:23:13 -0600

    Initial Commit
    Adding playgrounds to github

---
+++ OperationsOnVariables.playground/section-1.swift
@@ -0,0 +1,9 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var x = 49
+var y = 10
+
+// addition example
+var additionAnswer = x + y
---
+++ Variables.playground/section-1.swift
@@ -0,0 +1,13 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+/** var means the variable can change in the future
+    you can declare multiple variables on the same line
+    you do not need to say what type of variable it is (implicit)
+**/
+var firstInteger = 0, secondInteger = 29
+var thirdInteger:Int = -1000858 // explicitly declaring a variable
+var x = 5
+
+var discountAtDepartmentStore = 0.3 // double: a number with a decimal
